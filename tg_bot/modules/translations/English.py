ab7a03fbd2bf4bb2021cb6786a71c0da4d9df448
Muttahir <38470944+Muttahir786@users.noreply.github.com>
2019-05-18 23:12:47 +0800

    Update English.py

--- tg_bot/modules/translations/English.py
+++ tg_bot/modules/translations/English.py
@@ -126,7 +126,7 @@ SLAP_TEMPLATES = (
     "{user1} pins {user2} down and repeatedly {hits} them with a {item}.",
     "{user1} grabs up a {item} and {hits} {user2} with it.",
     "{user1} ties {user2} to a chair and {throws} a {item} at them.",
-    "{user1} gave a friendly push to help {user2} learn to swim in lava."
+    "{user1} gave a friendly push to help {user2} learn to swim in lava.",
 '   "{user2} was shot by {user1}.",
     "{user2} was pricked to death.",
     "{user2} walked into a cactus while trying to escape {user1}.",
