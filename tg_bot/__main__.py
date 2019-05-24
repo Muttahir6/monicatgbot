de991943f02e49a22e2496b79acdd866da4265bd
Muttahir <38470944+Muttahir786@users.noreply.github.com>
2019-05-25 02:34:21 +0800

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -26,10 +26,10 @@ I'm a modular group management bot maintained by [this wonderful person](https:/
 
 I'm a group manager bot . I'm built in python3, using the python-telegram-bot library,
 
-Click Help button or use /help to find out more about how to use me to my full potential.\n\nWant to add me to your group? [Click here!](t.me/Monicabetarobot?startgroup=true)"""
+Click Help button or use /help to find out more about how to use me to my full potential.\n\nWant to add me to your group? [Click here!](t.me/Monicarobot?startgroup=true)"""
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
+It took lots of work for [my creator](t.me/Muttahir) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \. 
 https://orangefox.tech/donate."""
 
