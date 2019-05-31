2bf6fdb7934d6bbfe78ecc7e1bf194cdb893a11e
Muttahir <38470944+Muttahir786@users.noreply.github.com>
2019-05-31 08:54:48 +0800

    Finally

--- tg_bot/modules/welcome.py
+++ tg_bot/modules/welcome.py
@@ -99,7 +99,11 @@ def new_member(bot: Bot, update: Update):
                     MESSAGE_DUMP,
                     "I have been added to {} with ID: <pre>{}</pre>".format(chat.title, chat.id),
                     parse_mode=ParseMode.HTML
-                )
+                )	                )
+                bot.send_message(	
+                    update.message.chat_id,	
+                    "Thanks For Adding Me Sweetheart."	
+                )	
                 continue
 
             else:
