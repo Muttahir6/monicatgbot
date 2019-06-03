3972c6c369891447454ef22b4fe50e0ea3b0b4c5
Muttahir <38470944+Muttahir786@users.noreply.github.com>
2019-06-03 10:58:31 +0800

    Removed useless things

--- tg_bot/modules/extra.py
+++ tg_bot/modules/extra.py
@@ -8,177 +8,7 @@ from tg_bot import dispatcher
 from tg_bot.modules.disable import DisableAbleCommandHandler
 
 
-EYES = [
-    ['⌐■', '■'],
-    [' ͠°', ' °'],
-    ['⇀', '↼'],
-    ['´• ', ' •`'],
-    ['´', '`'],
-    ['`', '´'],
-    ['ó', 'ò'],
-    ['ò', 'ó'],
-    ['⸌', '⸍'],
-    ['>', '<'],
-    ['Ƹ̵̡', 'Ʒ'],
-    ['ᗒ', 'ᗕ'],
-    ['⟃', '⟄'],
-    ['⪧', '⪦'],
-    ['⪦', '⪧'],
-    ['⪩', '⪨'],
-    ['⪨', '⪩'],
-    ['⪰', '⪯'],
-    ['⫑', '⫒'],
-    ['⨴', '⨵'],
-    ['⩿', '⪀'],
-    ['⩾', '⩽'],
-    ['⩺', '⩹'],
-    ['⩹', '⩺'],
-    ['◥▶', '◀◤'],
-    ['◍', '◎'],
-    ['/͠-', '┐͡-\\'],
-    ['⌣', '⌣”'],
-    [' ͡⎚', ' ͡⎚'],
-    ['≋'],
-    ['૦ઁ'],
-    ['  ͯ'],
-    ['  ͌'],
-    ['ළ'],
-    ['◉'],
-    ['☉'],
-    ['・'],
-    ['▰'],
-    ['ᵔ'],
-    [' ﾟ'],
-    ['□'],
-    ['☼'],
-    ['*'],
-    ['`'],
-    ['⚆'],
-    ['⊜'],
-    ['>'],
-    ['❍'],
-    ['￣'],
-    ['─'],
-    ['✿'],
-    ['•'],
-    ['T'],
-    ['^'],
-    ['ⱺ'],
-    ['@'],
-    ['ȍ'],
-    ['  '],
-    ['  '],
-    ['x'],
-    ['-'],
-    ['$'],
-    ['Ȍ'],
-    ['ʘ'],
-    ['Ꝋ'],
-    [''],
-    ['⸟'],
-    ['๏'],
-    ['ⴲ'],
-    ['◕'],
-    ['◔'],
-    ['✧'],
-    ['■'],
-    ['♥'],
-    [' ͡°'],
-    ['¬'],
-    [' º '],
-    ['⨶'],
-    ['⨱'],
-    ['⏓'],
-    ['⏒'],
-    ['⍜'],
-    ['⍤'],
-    ['ᚖ'],
-    ['ᴗ'],
-    ['ಠ'],
-    ['σ'],
-    ['☯']
-]
 
-MOUTHS = [
-    ['v'],
-    ['ᴥ'],
-    ['ᗝ'],
-    ['Ѡ'],
-    ['ᗜ'],
-    ['Ꮂ'],
-    ['ᨓ'],
-    ['ᨎ'],
-    ['ヮ'],
-    ['╭͜ʖ╮'],
-    [' ͟ل͜'],
-    [' ͜ʖ'],
-    [' ͟ʖ'],
-    [' ʖ̯'],
-    ['ω'],
-    [' ³'],
-    [' ε '],
-    ['﹏'],
-    ['□'],
-    ['ل͜'],
-    ['‿'],
-    ['╭╮'],
-    ['‿‿'],
-    ['▾'],
-    ['‸'],
-    ['Д'],
-    ['∀'],
-    ['!'],
-    ['人'],
-    ['.'],
-    ['ロ'],
-    ['_'],
-    ['෴'],
-    ['ѽ'],
-    ['ഌ'],
-    ['⏠'],
-    ['⏏'],
-    ['⍊'],
-    ['⍘'],
-    ['ツ'],
-    ['益'],
-    ['╭∩╮'],
-    ['Ĺ̯'],
-    ['◡'],
-    [' ͜つ']
-]
-
-EARS = [
-    ['q', 'p'],
-    ['ʢ', 'ʡ'],
-    ['⸮', '?'],
-    ['ʕ', 'ʔ'],
-    ['ᖗ', 'ᖘ'],
-    ['ᕦ', 'ᕥ'],
-    ['ᕦ(', ')ᕥ'],
-    ['ᕙ(', ')ᕗ'],
-    ['ᘳ', 'ᘰ'],
-    ['ᕮ', 'ᕭ'],
-    ['ᕳ', 'ᕲ'],
-    ['(', ')'],
-    ['[', ']'],
-    ['¯\\_', '_/¯'],
-    ['୧', '୨'],
-    ['୨', '୧'],
-    ['⤜(', ')⤏'],
-    ['☞', '☞'],
-    ['ᑫ', 'ᑷ'],
-    ['ᑴ', 'ᑷ'],
-    ['ヽ(', ')ﾉ'],
-    ['\\(', ')/'],
-    ['乁(', ')ㄏ'],
-    ['└[', ']┘'],
-    ['(づ', ')づ'],
-    ['(ง', ')ง'],
-    ['⎝', '⎠'],
-    ['ლ(', 'ლ)'],
-    ['ᕕ(', ')ᕗ'],
-    ['(∩', ')⊃━☆ﾟ.*'],
-]
 
 TOSS = (
     "Heads",
@@ -198,20 +28,6 @@ def shrug(bot: Bot, update: Update):
   reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
   reply_text("¯\_(ツ)_/¯")	
 	
-@run_async
-def bluetext(bot: Bot, update: Update):
-  # reply to correct message
-  reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
-  reply_text("BLUE TEXT\n MUST CLICK\n I AM A STUPID ANIMAL THAT IS ATTRACTED TO COLORS")		
-
-@run_async
-def rlg(bot: Bot, update: Update):
-  # reply to correct message
-  eyes = random.choice(EYES)
-  mouth = random.choice(MOUTHS)
-  ears = random.choice(EARS)
-  repl = format(ears + eyes + mouth + eyes + ears)
-  update.message.reply_text(repl)
 	
 def decide(bot: Bot, update: Update):
       r = randint(1, 100)
@@ -238,7 +54,6 @@ __help__ = """
  - /toss : Tosses A coin
  - /bluetext : check urself :V
  - /roll : Roll a dice.
- - /rlg : Join ears,nose,mouth and create an emo ;-;
 """
 
 __mod_name__ = "Extras"
@@ -246,15 +61,11 @@ __mod_name__ = "Extras"
 ROLL_HANDLER = DisableAbleCommandHandler("roll", roll)
 TOSS_HANDLER = DisableAbleCommandHandler("toss", toss)
 SHRUG_HANDLER = DisableAbleCommandHandler("shrug", shrug)
-BLUETEXT_HANDLER = DisableAbleCommandHandler("bluetext", bluetext)
-RLG_HANDLER = DisableAbleCommandHandler("rlg", rlg)
 DECIDE_HANDLER = DisableAbleCommandHandler("decide", decide)
 TABLE_HANDLER = DisableAbleCommandHandler("table", table)
 
 dispatcher.add_handler(ROLL_HANDLER)
 dispatcher.add_handler(TOSS_HANDLER)
 dispatcher.add_handler(SHRUG_HANDLER)
-dispatcher.add_handler(BLUETEXT_HANDLER)
-dispatcher.add_handler(RLG_HANDLER)
 dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
